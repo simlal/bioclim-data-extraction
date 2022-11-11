b0a379b47e1ec7dc1e577a42626e2985c7489d52
simlal <simlalonde@hotmail.com>
2022-11-11 16:26:46 -0500

    Start base wget and url-parser func

--- scripts/chelsa_bio_links.py
+++ scripts/chelsa_bio_links.py
@@ -1,3 +1,36 @@
-import bs4
+import yaml
+import subprocess
+import requests
 
-# https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V2/GLOBAL/climatologies/1981-2010/bio/CHELSA_ai_1981-2010_V.2.1.tif
+# Load yaml and set vars
+
+with open('../config.yaml') as f:
+    config = yaml.safe_load(f)
+
+chelsa_urlfile = config["url-files"]["chelsa"]
+worldclim_urlfile = config["url-files"]["worldclim"]
+test_urlfile = config["url-files"]["test"]
+
+# wget with subprocess
+def wget_py():
+    wget = subprocess.Popen(['wget', 'https://www.w3schools.com/images/colorpicker2000.pngg'], stdout=subprocess.PIPE, text=True)
+    if wget.stderr == 0 :
+        print(wget.stdout)
+    else : print(wget.stderr)
+    
+
+
+# Parse data
+def parse_urlfile(urlfile):
+    with open(urlfile) as f:
+        urls = [line.rstrip() for line in f]
+        for url in urls : 
+            response = requests.get(url)
+            if response.status_code == 200 : 
+                print(url)
+            else :
+                print("URL {} cannot be downloaded. Status code : {}".format(url,response.status_code))
+
+parse_urlfile(test_urlfile)
+
+# Multiprocessing/parralel download
