033e39f7fb900b06e315dc6f65f632f30e915408
simlal <simlalonde@hotmail.com>
2022-11-15 15:54:24 -0500

    Download zip and progress draft

--- scripts/bioclim_download.py
+++ scripts/bioclim_download.py
@@ -1,36 +1,46 @@
 import yaml
-import subprocess
 import requests
+import time
+import sys
+# from tqdm.auto import tqdm
 
 # Load yaml and set vars
 
-with open('../config.yaml') as f:
+with open('config.yaml') as f:
     config = yaml.safe_load(f)
 
-chelsa_urlfile = config["url-files"]["chelsa"]
-worldclim_urlfile = config["url-files"]["worldclim"]
-test_urlfile = config["url-files"]["test"]
-
-# wget with subprocess
-def wget_py():
-    wget = subprocess.Popen(['wget', 'https://www.w3schools.com/images/colorpicker2000.pngg'], stdout=subprocess.PIPE, text=True)
-    if wget.stderr == 0 :
-        print(wget.stdout)
-    else : print(wget.stderr)
-    
+chelsa_urls = config["url-files"]["chelsa"]
+worldclim_urls = config["url-files"]["worldclim"]
+test_urls = config["url-files"]["test"]
+download_path = config["download-path"]
 
 
-# Parse data
-def parse_urlfile(urlfile):
+# Download zip files
+def download_zip(urlfile, filepath):
     with open(urlfile) as f:
         urls = [line.rstrip() for line in f]
-        for url in urls : 
-            response = requests.get(url)
+        for url in urls :
+            filename = url.split("/")[-1]
+            response = requests.get(url, stream=True)
+            # Check file size
+            total_size = float(response.headers['Content-Length'])
+            print("{} is {:.1f} MB".format(filename, total_size/1000000))
+            # Download in chunks of 1 MB
             if response.status_code == 200 : 
-                print(url)
+                print("Server response OK from {}, Downloading {}".format(''.join(url.split(".")[:3]), filename))
+                with open(filename, 'wb') as f :
+                    chunksize = 1024 * 1000
+                    for n, chunk in enumerate(response.iter_content(chunk_size=chunksize)) :
+                        percent = (n * chunksize / total_size) * 100 
+                        sys.stdout.write("\r{:.2f} % of {:.1f} MB  downloaded".format(percent, total_size/1000000))
+                        time.sleep(0.1)
+                        sys.stdout.flush()
+                        f.write(chunk)
             else :
-                print("URL {} cannot be downloaded. Status code : {}".format(url,response.status_code))
+                print("File {} cannot be downloaded. Status code : {}".format(filename, response.status_code))
+            
+            return filepath+filename
 
-parse_urlfile(test_urlfile)
+download_zip(test_urls, download_path)
 
 # Multiprocessing/parralel download
