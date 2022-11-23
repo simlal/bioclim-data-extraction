1f54cf8ae076454b4db9f976656709b16164042c
simlal <simlalonde@hotmail.com>
2022-11-23 12:00:31 -0500

    Initalize SingleCoords class

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -6,31 +6,68 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Read dataset (use with open() for proper open/close cycle)
-with rasterio.open("../data/CHELSA_bio2_1981-2010_V.2.1.tif", masked=True) as small :
-    print("boundaries : {}".format(small.bounds))
-    sm = small.read()
+class SingleCoords :
+    """
+    A class to a single data point under geographic coordinate system standard conventions (EPSG:4326).
+
+    ...
+
+    Attributes
+    ----------
+    id : str
+        identifier of single data point
+    lat : float
+        latitude (y-value, East-West lines) that range between -90 and +90 degrees.
+    lon : float
+        latitude (x-value, North-South lines) that range between -90 and +90 degrees.
+
+    Methods
+    -------
+    info():
+        Prints the SingleCoords information
+    """
+    def __init__(self, id, lat, lon) :
+        """
+        Constructor for SingleCoords object.
+
+        Parameters
+        ----------
+        id : str
+            identifier of single data point
+        lat : float
+            latitude (y-value, East-West lines) that range between -90 and +90 degrees.
+        lon : float
+            latitude (x-value, North-South lines) that range between -90 and +90 degrees.
+        """
+        self.id = id
+        self.lat = lat
+        self.lon = lon
+
+    def info(self) :
+        """
+        Prints the SingleCoords object information.
+        """
+        print('id : {}\nEPSG:4326 / GPS coordinates :\n\tlat = {}\n\tlon = {}'.format(self.id, self.lat, self.lon))
+
+
+#Read dataset (use with open() for proper open/close cycle)
+with rasterio.open("../data/CHELSA_bio2_1981-2010_V.2.1.tif", masked=True) as dat :
+    print("boundaries : {}".format(dat.bounds))
+    data = dat.read()
     
 
     #transform GPS coords to raster EPSG  (which is the same in this case is 4326 so the same...)
     latlon = [(45.508888, -73.561668), (48.864716, 2.349014)]     # montreal + paris in Lat + Lon form
-    transformer = Transformer.from_crs("epsg:4326", small.crs)
+    transformer = Transformer.from_crs("epsg:4326", dat.crs)
     coords = [transformer.transform(x,y) for x,y in latlon]
 
     print(coords)
 
     #filter array ?
-    # sm_filt = np.where(sm<5000, np.nan, sm)
+    # data_filt = np.where(data<5000, np.nan, data)
     
     # Extract data from transformed locations
     #* Beware of the correction factor + offset in Chelsa
-    vals = list(rasterio.sample.sample_gen(small, coords))
+    vals = list(rasterio.sample.sample_gen(dat, coords))
     for (lon,lat), val in zip(coords, vals):
         print(lon,lat,val[0])
-
-    #Plot with colorbar
-    sm_res = rasterio.plot.reshape_as_image(sm) # reshape arr as img
-    fig, ax = plt.subplots(figsize=(10,10))
-    img = plt.imshow(sm_res)
-    fig.colorbar(img, ax=ax)
-    plt.show()
