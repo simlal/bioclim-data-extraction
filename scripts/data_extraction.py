5d218db43052f3dfeae937f25808b9040161d42e
simlal <simlalonde@hotmail.com>
2022-12-02 12:37:32 -0500

    rm bad doctest + include transform in df_to_dict

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -51,9 +51,9 @@ class CrsDataPoint :
         epsg : int
             EPSG Geodetic Parameter Dataset code of the coordinate reference system (CRS)
         x : float
-            x-value (equivalent of East-West latitude lines) of the CRS
+            x-value (equivalent of East-West longitude lines for EPSG:4326) of the CRS
         y : float
-            y-value (equivalent of North-Sound longitude lines) of the CRS
+            y-value (equivalent of North-Sound latitude lines for EPSG:4326) of the CRS
         xy_pt : tuple
             x,y values in tuple format
 
@@ -92,7 +92,7 @@ class CrsDataPoint :
         >>> 
         """
         return(
-            'id : {}\nEPSG : {}\nMap coordinates :\n\tx = {}\n\ty = {}\n###########################'
+            'id : {}\nEPSG : {}\nMap coordinates :\n\tx = {}\n\ty = {}\n\t(x,y) = \n###########################'
             .format(self.id, self.epsg, self.x, self.y)
             )
 
@@ -100,7 +100,6 @@ class CrsDataPoint :
     def transform_GPS(self, epsg_out=4326) :
         """
         Transforms the coordinates to the desired EPSG coordinate reference system. 
-        
         Default argument is EPSG:4326 which is the reference for the WorldClim and Chelsa datasets
         
         Parameters
@@ -118,33 +117,16 @@ class CrsDataPoint :
             latitude (y-value, East-West lines) that range between -90 and +90 degrees.
 
 
-        >>> from data_extraction import CrsDataPoint
-        >>> coord = CrsDataPoint('c1', 3005, 4760950.613410814, 1516444.4032389917)
-        >>> print(coord)
-        id : c1
-        EPSG : 3005
-        Map coordinates :
-            x = 4760950.613410814
-            y = 1516444.4032389917
-        ###########################
-        >>> coord_transformed = coord.transform_GPS()
-        >>> print(coord_transformed)
-        id : c1_transformed
-        EPSG : 4326
-        Map coordinates :
-            x = 45.508888000000034
-            y = -73.56166799999998
-        ###########################
 
         """
         transformer = Transformer.from_crs(self.epsg, CRS.from_epsg(epsg_out))
         x_out, y_out = transformer.transform(self.x, self.y)
-        # return (x_out, y_out)
         return CrsDataPoint(self.id+"_transformed", epsg_out, x_out, y_out)
 
-    def df_to_dict(dataframe) :
+    def df_to_dict(df) :
         """
-        Takes the input df and returns a dictionnary of CrsDataPoint objects
+        Takes the input df and returns a dictionnary of CrsDataPoint objects.
+        Calls transform_GPS() method if any epsg codes are not 4326.
 
         Parameters
         ----------
@@ -154,34 +136,27 @@ class CrsDataPoint :
         -------
         Dictionnary of CrsDataPoint objects
 
-        >>> df = pd.DataFrame({
-        ... 'id' : ['montreal', 'paris'],
-        ... 'epsg' : [4326,3005],
-        ... 'x' : [45.508888, 48.864716],
-        ... 'y' : [-73.561668, 2.349014],
-        ... })
-        >>> crs_data_points = CrsDataPoint.df_to_dict(df)
-        >>> print(crs_data_points)
-        {'montreal': <data_extraction.CrsDataPoint object at 0x7f561ab6a310>, 'paris': <data_extraction.CrsDataPoint object at 0x7f561ab68a90>}
-        >>> print(crs_data_points['montreal'])
-        id : montreal
-        EPSG : 4326
-        Map coordinates :
-            x = 45.508888
-            y = -73.561668
-        ###########################
+        
 
         """
         crs_data_points = {}
-        for index, row in dataframe.iterrows() :
-            crs_data_points[row['id']] = CrsDataPoint(row['id'], row['epsg'], row['x'], row['y'])
-        return crs_data_points
+        if (df['epsg'] != 4326).any() == True :
+            print("Dataframe contains data with CRS other than EPSG:4326. Calling transform_GPS()...")
+            for index, row in df.iterrows() :
+                if row['epsg'] == 4326 : 
+                    crs_data_points[row['id']] = CrsDataPoint(row['id'], row['epsg'], row['x'], row['y'])
+                else : 
+                    crs_data_points[row['id']] = CrsDataPoint(row['id'], row['epsg'], row['x'], row['y']).transform_GPS()
+            return crs_data_points
+        else :     
+            for index, row in df.iterrows() :
+                crs_data_points[row['id']] = CrsDataPoint(row['id'], row['epsg'], row['x'], row['y'])
+            return crs_data_points
 
     def single_point_extraction():
         """
         
         """
-
 
 mont = CrsDataPoint('montreal', 4326, 45.508888, -73.561668)
 with rasterio.open('./data/CHELSA_bio2_1981-2010_V.2.1.tif') as dat:
@@ -197,11 +172,13 @@ with rasterio.open('./data/CHELSA_bio2_1981-2010_V.2.1.tif') as dat:
 
 df = pd.DataFrame({
     'id' : ['montreal', 'paris'],
-    'epsg' : [4326,3005],
+    'epsg' : [4326,3857],
     'x' : [45.508888, 48.864716],
     'y' : [-73.561668, 2.349014],
         })
+print(df)
 crs_data_points = CrsDataPoint.df_to_dict(df)
+print(crs_data_points['paris'])
 
 with rasterio.open('./data/CHELSA_bio2_1981-2010_V.2.1.tif') as dat:
     xy_pts = [v.xy_pt for k,v in crs_data_points.items()]
@@ -216,3 +193,4 @@ with rasterio.open('./data/CHELSA_bio2_1981-2010_V.2.1.tif') as dat:
             xy[0], xy[1], xy, clim[0]
             ]
     print(df_test)
+
